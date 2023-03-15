4affc64c90c327cb02bb6e44429d275486013659
Prathamesh Ghorpade <ghorpadeprathamesh2411@gmail.com>
2023-03-15 13:37:33 +0530

    api url change

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -12,17 +12,20 @@ const SignUp = () => {
   let navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch(`http://localhost:5000/api/auth/createuser`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name: credentials.name,
-        email: credentials.email,
-        password: credentials.password,
-      }),
-    });
+    const response = await fetch(
+      `https://thankful-lime-twill.cyclic.app/api/auth/createuser`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: credentials.name,
+          email: credentials.email,
+          password: credentials.password,
+        }),
+      }
+    );
 
     const json = await response.json();
     console.log(json);
